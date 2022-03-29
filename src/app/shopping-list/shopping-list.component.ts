cfd0b817d8a00a3fb83a1c512686efc0bc85d3ed
davidsqui <davidsqr6@gmail.com>
2022-03-29 16:12:08 -0500

    feat: create initial components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,22 @@
-import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ingredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
-
 }
